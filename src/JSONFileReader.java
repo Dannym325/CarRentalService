33592ad241b0ad6d1bfd0b56c03e40d1bf138da7
u1477073 <u1477073@unimail.hud.ac.uk>
2017-08-01 20:38:48 +0100

    Adding JSON jar files
    Adding JAR files to help read in JSON files. Added a new file reader
    class

---
+++ src/JSONFileReader.java
@@ -0,0 +1,19 @@
+import org.json.simple.parser.JSONParser;
+
+/**
+ * 
+ * This class will be used to read JSON files into the Java project
+ * 
+ * @author Daniel Madeley
+ *
+ */
+
+public class JSONFileReader {
+
+	public static void main(String[] args) {
+		
+		JSONParser parser = new JSONParser();
+		
+	}
+
+}
